6a684a3c766f850326f0cb47fec2c71ebab9919c
AaronConstant <aconstant@pursuit.org>
2024-09-20 16:07:53 -0400

    Added routes for frontend pages to the best of my knowledge. awaiting for input from Manoel/Rizel

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Network from "./components/Network";
-import Navbar from "./components/Navbar.jsx";
-import Home from "./components/Home.jsx";
-import AboutUs from "./components/AboutUs.jsx";
 import "./App.css";
-
-const App = () => {
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import Home from "./Pages/Home";
+function App() {
   return (
     <>
-      <Header /> {/* Call to Action Header for Services */}
+      <Header />
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path='/login' element={<Login/>}/> */}
-        {/* <Route path='/signup' element={<SignUp/>}/> */}
-        <Route path="/AboutUs" element={<AboutUs />} />
-        <Route path="/lenders/:id/lenderdashboard" element={<Lenderpage />} />
-        <Route path="/borrowers/:id/borrowersdashboard" element={<></>} />
-        <Route path="/lenders/:id/edit" element={<></>} />
-        <Route path="/borrowers/:id/edit" element={<></>} />
-        <Route path="lenders/:id/proposals" element={<></>} />
-        <Route path="lenders/:id/proposals/:id" element={<></>} />
-        <Route path="borrowers/:id/requests/:id" element={<></>} />
-        <Route path="/disclaimer" element={<></>} />{" "}
-        {/* FF: Info page for Borrowers/Lenders to know about additional services */}
       </Routes>
     </>
   );
-};
+}
 
 export default App;
