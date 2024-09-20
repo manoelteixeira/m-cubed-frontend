f4dd458428640a9df99dcce73d3513fc888cb6c8
AaronConstant <aconstant@pursuit.org>
2024-09-20 09:09:09 -0400

    Created the Navbar on the Homepage Branch/No CSS/It's Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
-import LenderDashboard from './components/LenderDashboard/LenderDashboard'
-import { Routes,Route } from 'react-router-dom'
-import NavBar from './components/NavBar'
+
 function App() {
 
   return (
     <>
     <LenderDashboard/>
+    <NavBar/>
     <NavBar/>
     </>
   )
