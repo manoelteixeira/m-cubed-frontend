0456f13c9c0c8f67d4a9ce5bc2e92a9f19589a0d
AaronConstant <aconstant@pursuit.org>
2024-09-21 11:52:31 -0400

    Added sass styling to lenderdashboard to gauge spacing

--- src/components/LenderDashboard/LenderDashboard.jsx
+++ src/components/LenderDashboard/LenderDashboard.jsx
@@ -1,4 +1,7 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
 import "./LenderDashboard.scss";
+const API = import.meta.env.VITE_BASE_URL;
 
 export default function LenderDashboard() {
   const { id } = useParams();
