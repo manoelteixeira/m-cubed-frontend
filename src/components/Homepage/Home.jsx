fcfce0a6e71672e4b2c97eefa17a3a7d8f054f31
AaronConstant <aconstant@pursuit.org>
2024-09-23 21:45:32 -0400

    styling of button and borrower banner on homepage

--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -11,23 +11,28 @@ export default function Home() {
         <img src="" alt="possible-img" id='' />
       </section>
       <section className='home-container__badge-names'>
-        <h2>Sample 1</h2> {/* Horizontal display */}
-        <h2>Sample 2</h2>
-        <h2>Sample 3</h2>
-        <h2>Sample 4</h2>
-        <h2>Sample 5</h2>
+        <h2>Sponsors 1</h2> {/* Horizontal display */}
+        <h2>Sponsors 2</h2>
+        <h2>Sponsors 3</h2>
+        <h2>Sponsors 4</h2>
+        <h2>Sponsors 5</h2>
       </section>
       {/*Hopscotch Patch work display */}
       <section className='home-container__lender-CTA'>
-        <p className='lender-details'>Diversify your Portfolio by Partnering with M-cubed for endless Possibilites</p>
+        <div className='details-container'>
+            <p className='lender-details'>Diversify your Portfolio by Partnering with M-cubed for endless Possibilites</p>
+            <button className='banner-signup'>Sign up Now</button>
+        </div>
         <img src={ LenderPhoto } alt="borrower copilot pic" id='lender-photo' />
       </section>
       <section className='home-container__borrower-CTA'>
         <img src={ BorrowerPhoto } alt="borrwer copilot pic" id='borrower-photo'/>
-        <p>Lender Information</p>
+        <div>
+        <p>Small </p>
+        <p className='borrower-details'> <span>Apply</span> for faster funding for qualifying loan requests</p>
+        </div>
       </section>
       <section className='home-container__container-5'>
-
       </section>
     </main>
   )
