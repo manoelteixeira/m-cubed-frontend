8a069472d5d5099ec950cd17b4bd7562e14cc7c9
AaronConstant <aconstant@pursuit.org>
2024-09-28 20:50:57 -0400

    changes to LenderDashboard working on grid

--- src/components/LenderDashboard/LenderDashboard.jsx
+++ src/components/LenderDashboard/LenderDashboard.jsx
@@ -1,8 +1,48 @@
 
 import React from 'react';
-import {AppBar,Toolbar,Typography,Button,TextField,Select,MenuItem,FormControl,InputLabel,
-  Table,TableBody,TableCell,TableContainer,TableHead,TableRow,Paper,
+import {AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  TextField,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
 } from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Stack from '@mui/material/Stack';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import AnalyticsRoundedIcon from '@mui/icons-material/AnalyticsRounded';
+import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
+import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
+import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
+
+const mainListItems = [
+  { text: 'Home', icon: <HomeRoundedIcon /> },
+  { text: 'Analytics', icon: <AnalyticsRoundedIcon /> },
+  { text: 'Clients', icon: <PeopleRoundedIcon /> },
+  { text: 'Tasks', icon: <AssignmentRoundedIcon /> },
+];
+
+const secondaryListItems = [
+  { text: 'Settings', icon: <SettingsRoundedIcon /> },
+  { text: 'About', icon: <InfoRoundedIcon /> },
+  { text: 'Feedback', icon: <HelpRoundedIcon /> },
+];
 import './LenderDashboard.scss';
 // import logo from "../../assets/logo.jpeg";
 
@@ -25,10 +65,10 @@ export default function LenderDashboard({ userlenderData }) {
       </Typography>
       <div className="total-loan-volume">
         <Typography variant="h6">
-          Total Loan Volume: <span>{/* helper function for total Loan volumes */}</span>
+          Total Loan Volume: <span>helper function for total Loan volumes</span>
         </Typography>
       </div>
-
+      {/* 
       <div className="filters" style={{ marginTop: 2, marginBottom: 2 }}>
         <TextField
           label="Search by loan # or name"
@@ -40,13 +80,12 @@ export default function LenderDashboard({ userlenderData }) {
           <InputLabel>Loan Status</InputLabel>
           <Select defaultValue="11">
             <MenuItem value="11">Loans Selected</MenuItem>
-            {/* Add more MenuItems as needed */}
           </Select>
         </FormControl>
         <Button variant="contained" color="secondary">
           Reset Filters
         </Button>
-      </div>
+      </div> */}
 
       <TableContainer component={Paper}>
         <Table>
@@ -66,6 +105,31 @@ export default function LenderDashboard({ userlenderData }) {
           </TableBody>
         </Table>
       </TableContainer>
+
+      <Stack sx={{ flexGrow: 1, p: 1, justifyContent: 'space-between' }}>
+      <List dense>
+        {mainListItems.map((item, index) => (
+          <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+            <ListItemButton selected={index === 0}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+
+      <List dense>
+        {secondaryListItems.map((item, index) => (
+          <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+            <ListItemButton>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Stack>
     </div>
+    
   );
 }
