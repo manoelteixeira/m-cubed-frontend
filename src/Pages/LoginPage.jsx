a9e067c8e9d7bfc8065923e49bb1d281bf2469f8
kbodur <kbodur@pursuit.org>
2024-10-02 15:28:55 -0400

    rebase dev to Login branch

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import Login from '../components/Library/AppFiles/Login'
+import Login from '../components/Login/Login'
 
 export const LoginPage = () => {
   return (
     <div>
-      LoginPage
+      Log in
       <Login />
     </div>
   )
 }
+
