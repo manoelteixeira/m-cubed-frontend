04ac21b34254fdb4fde274b1fa27f25f9f2d6955
AaronConstant <aconstant@pursuit.org>
2024-10-09 12:07:21 -0400

    Updates to the dashboard

--- src/Pages/Lenderpage.jsx
+++ src/Pages/Lenderpage.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
-import { useParams } from 'react-router';
 import LenderDashboard from '../components/LenderDashboard/LenderDashboard';
 
 export default function Lenderpage() {
-
-  const { id } = useParams()
-  
-
   return (
     <div>
       <LenderDashboard/>
