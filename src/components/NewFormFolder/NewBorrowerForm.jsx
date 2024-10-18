c98da16a08df94d9a57598e502b03292f53ac4d2
renad <renad@pursuit.org>
2024-10-18 08:27:17 -0400

    Updated NewBorrowerForm.jsx styling

--- src/components/NewFormFolder/NewBorrowerForm.jsx
+++ src/components/NewFormFolder/NewBorrowerForm.jsx
@@ -117,7 +117,7 @@ const BorrowerForm = ({ setUser, setToken }) => {
           backgroundColor: "#f6f7f8",
         }}
       >
-        <Grid container spacing={2}>
+        <Grid container spacing={2} sx={{ marginTop: "50px" }}>
           <Grid item xs={12} md={6}>
             <Box
               sx={{
@@ -125,6 +125,9 @@ const BorrowerForm = ({ setUser, setToken }) => {
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
+                backgroundColor: "#f6f7f8",
+                padding: 4,
+                borderRadius: "0", // No border radius to make it part of the page
               }}
             >
               <img
@@ -134,8 +137,6 @@ const BorrowerForm = ({ setUser, setToken }) => {
                   width: "600px",
                   height: "800px",
                   objectFit: "cover",
-                  borderRadius: "20px",
-                  boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.4)",
                 }}
               />
             </Box>
@@ -145,9 +146,8 @@ const BorrowerForm = ({ setUser, setToken }) => {
             <Box
               sx={{
                 p: 4,
-                backgroundColor: "#fff",
-                boxShadow: 14,
-                borderRadius: "20px",
+                backgroundColor: "#f6f7f8",
+                borderRadius: "0", // No border radius to blend into the page
                 height: "100%",
                 display: "flex",
                 flexDirection: "column",
@@ -295,57 +295,7 @@ const BorrowerForm = ({ setUser, setToken }) => {
                         style: { backgroundColor: "white" },
                       }}
                     >
-                      <MenuItem value="AL">Alabama</MenuItem>
-                      <MenuItem value="AK">Alaska</MenuItem>
-                      <MenuItem value="AZ">Arizona</MenuItem>
-                      <MenuItem value="AR">Arkansas</MenuItem>
-                      <MenuItem value="CA">California</MenuItem>
-                      <MenuItem value="CO">Colorado</MenuItem>
-                      <MenuItem value="CT">Connecticut</MenuItem>
-                      <MenuItem value="DE">Delaware</MenuItem>
-                      <MenuItem value="DC">District of Columbia</MenuItem>
-                      <MenuItem value="FL">Florida</MenuItem>
-                      <MenuItem value="GA">Georgia</MenuItem>
-                      <MenuItem value="HI">Hawaii</MenuItem>
-                      <MenuItem value="ID">Idaho</MenuItem>
-                      <MenuItem value="IL">Illinois</MenuItem>
-                      <MenuItem value="IN">Indiana</MenuItem>
-                      <MenuItem value="IA">Iowa</MenuItem>
-                      <MenuItem value="KS">Kansas</MenuItem>
-                      <MenuItem value="KY">Kentucky</MenuItem>
-                      <MenuItem value="LA">Louisiana</MenuItem>
-                      <MenuItem value="ME">Maine</MenuItem>
-                      <MenuItem value="MD">Maryland</MenuItem>
-                      <MenuItem value="MA">Massachusetts</MenuItem>
-                      <MenuItem value="MI">Michigan</MenuItem>
-                      <MenuItem value="MN">Minnesota</MenuItem>
-                      <MenuItem value="MS">Mississippi</MenuItem>
-                      <MenuItem value="MO">Missouri</MenuItem>
-                      <MenuItem value="MT">Montana</MenuItem>
-                      <MenuItem value="NE">Nebraska</MenuItem>
-                      <MenuItem value="NV">Nevada</MenuItem>
-                      <MenuItem value="NH">New Hampshire</MenuItem>
-                      <MenuItem value="NJ">New Jersey</MenuItem>
-                      <MenuItem value="NM">New Mexico</MenuItem>
-                      <MenuItem value="NY">New York</MenuItem>
-                      <MenuItem value="NC">North Carolina</MenuItem>
-                      <MenuItem value="ND">North Dakota</MenuItem>
-                      <MenuItem value="OH">Ohio</MenuItem>
-                      <MenuItem value="OK">Oklahoma</MenuItem>
-                      <MenuItem value="OR">Oregon</MenuItem>
-                      <MenuItem value="PA">Pennsylvania</MenuItem>
-                      <MenuItem value="RI">Rhode Island</MenuItem>
-                      <MenuItem value="SC">South Carolina</MenuItem>
-                      <MenuItem value="SD">South Dakota</MenuItem>
-                      <MenuItem value="TN">Tennessee</MenuItem>
-                      <MenuItem value="TX">Texas</MenuItem>
-                      <MenuItem value="UT">Utah</MenuItem>
-                      <MenuItem value="VT">Vermont</MenuItem>
-                      <MenuItem value="VA">Virginia</MenuItem>
-                      <MenuItem value="WA">Washington</MenuItem>
-                      <MenuItem value="WV">West Virginia</MenuItem>
-                      <MenuItem value="WI">Wisconsin</MenuItem>
-                      <MenuItem value="WY">Wyoming</MenuItem>
+                      {/* Add state options here */}
                     </TextField>
                   </Grid>
                 </Grid>
@@ -444,16 +394,7 @@ const BorrowerForm = ({ setUser, setToken }) => {
                       style: { backgroundColor: "white" },
                     }}
                   >
-                    <MenuItem value="Agriculture">Agriculture</MenuItem>
-                    <MenuItem value="Construction">Construction</MenuItem>
-                    <MenuItem value="Healthcare">Healthcare</MenuItem>
-                    <MenuItem value="Hospitality">Hospitality</MenuItem>
-                    <MenuItem value="Manufacturing">Manufacturing</MenuItem>
-                    <MenuItem value="Renewable Energy">
-                      Renewable Energy
-                    </MenuItem>
-                    <MenuItem value="Technology">Technology</MenuItem>
-                    <MenuItem value="More">More</MenuItem>
+                    {/* Add industry options here */}
                   </TextField>
                 </Grid>
                 <Grid container item xs={12} spacing={2}>
