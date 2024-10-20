96bf28faa75fb0f58a0bd96b3028e688d1c931f6
AaronConstant <aconstant@pursuit.org>
2024-10-20 17:07:12 -0400

    changes to components QR Code

--- src/components/QRCodeComponent.jsx
+++ src/components/QRCodeComponent.jsx
@@ -5,24 +5,20 @@ import { Container, Typography, Box, Link } from '@mui/material';
 const API = import.meta.env.VITE_BASE_URL;
 
 export default function QRCodeComponent() {
-
-
   return (
-    <Container>   
-        <Box>
-            <QRCode value={`${API}/newsletter`} size={100} bgColor={'#ffffff'} fgColor={'#000000'} level={'H'} />
+    <Container maxWidth="sm" sx={{ mt: 5, textAlign: 'center' }}>   
+        <Typography variant='h4' gutterBottom>
+            Join MMM Mailing List
+        </Typography>
+        <Typography variant='body1' gutterBottom>
+            Scan the QR code to subscribe:
+        </Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'center', my: 3 }}>
+            <QRCode value={`${API}/newsletter`} size={200} bgColor={'#ffffff'} fgColor={'#000000'} level={'H'} />
         </Box>
+        <Typography>
+            Or visit: <Link href={`${API}/newsletter`} target="_blank" rel="noreferrer">{`${API}/newsletter`}</Link>
+        </Typography>
     </Container>
   )
 }
-// Additional Code if we want to expand on other QRCODE page implications or options. 
-
- {/* <Typography variant='h4' gutterBottom>
-            Join MMM Mailing List and Stay in the know of our Recent Updates!
-        </Typography>
-        <Typography variant='body1' gutterBottom>
-            Scan the QR Provided:
-        </Typography> */}
-            {/* <Typography>
-        Or visit: <Link href={`${API}/newsletter`} target="_blank" rel="noreferrer">{`${API}/newsletter`}</Link>
-        </Typography> */}
