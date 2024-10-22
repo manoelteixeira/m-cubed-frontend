cc6db1a31336ce7ac4f375ffbd7bac409febfdd7
renad <renad@pursuit.org>
2024-10-22 00:47:27 -0400

    Fixed merge conflicts

--- src/components/QRCodeComponent.jsx
+++ src/components/QRCodeComponent.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import { QRCode } from 'react-qr-code'
-import { Container, Typography, Box, Link } from '@mui/material';
+import React from "react";
+import { QRCode } from "react-qr-code";
+import { Container, Typography, Box, Link } from "@mui/material";
 
 const API = import.meta.env.VITE_BASE_URL;
 
 export default function QRCodeComponent() {
   return (
-    <Container maxWidth="sm" sx={{ mt: 5, textAlign: 'center' }}>   
-        {/* <Typography variant='h4' gutterBottom>
+    <Container maxWidth="sm" sx={{ mt: 5, textAlign: "center" }}>
+      {/* <Typography variant='h4' gutterBottom>
             Join MMM Mailing List
         </Typography> */}
-        <Typography variant='body1' gutterBottom>
-           Or Scan the QR code:
-        </Typography>
-        <Box >
-            <QRCode value={`${API}/newsletter`} size={100} bgColor={'#ffffff'} fgColor={'#000000'} level={'H'} />
-        </Box>
-        {/* <Typography>
+      <Typography variant="body1" gutterBottom>
+        Or Scan the QR code:
+      </Typography>
+      <Box>
+        <QRCode
+          value={`${API}/newsletter`}
+          size={100}
+          bgColor={"#ffffff"}
+          fgColor={"#000000"}
+          level={"H"}
+        />
+      </Box>
+      {/* <Typography>
             Or visit: <Link href={`${API}/newsletter`} target="_blank" rel="noreferrer">{`${API}/newsletter`}</Link>
         </Typography> */}
     </Container>
-  )
+  );
 }
