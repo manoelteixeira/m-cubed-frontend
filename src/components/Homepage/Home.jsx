81bfb963aebdbbc0e761ca6b25486b3e69815c19
renad <renad@pursuit.org>
2024-10-22 00:46:41 -0400

    Fixed merge conflicts

--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -671,7 +671,6 @@ export default function Home() {
               border: "1px solid #e0e0e0",
               borderRadius: 2,
               backgroundColor: "fffff",
-              marginBottom: isMedium ? 3 : 0,
             }}
           >
             <Typography variant="h6" gutterBottom>
@@ -679,7 +678,6 @@ export default function Home() {
             </Typography>
             <Typography variant="h6" gutterBottom>
               Borrower Dashboard
-<<<<<<< HEAD
               <img
                 src={BorrowerDashboard}
                 alt="Product Placeholder"
@@ -693,33 +691,76 @@ export default function Home() {
                 alt="Product Placeholder"
                 style={{ width: "100%", borderRadius: 8, marginBottom: 16 }}
               />
-=======
-            <img
-              src={BorrowerDashboard}
-              alt="Product Placeholder"
-              style={{ width: "100%", borderRadius: 8, marginBottom: 16 }}
-            />
-            </Typography>
-            <Typography variant="h6" gutterBottom>
-              Lender Dashboard
-            <img
-              src={LenderDashboard}
-              alt="Product Placeholder"
-              style={{ width: "100%", borderRadius: 8, marginBottom: 16 }}
-            />
->>>>>>> 2c51ccb (BorrowerForm updates)
             </Typography>
           </Card>
         </Grid>
       </Grid>
+      <Grid container spacing={4} justifyContent="center" sx={{ padding: 5 }}>
+        <Grid item xs={12} display="flex" justifyContent="center">
+          <img
+            src="https://res.cloudinary.com/dxeoesm7e/image/upload/v1728576656/How_This_Works_2_tg9j17.png"
+            alt="Why Choose MMM"
+            style={{ width: "100%", maxWidth: 1200, borderRadius: 8 }}
+          />
+        </Grid>
 
-      {/* How This Works Section */}
-      <Grid
-        container
-        spacing={isMobile ? 2 : isMedium ? 3 : 4}
-        justifyContent="center"
-        sx={{ padding: isMobile ? 2 : isMedium ? 3 : 5 }}
-      >
+        <Grid container justifyContent="center" spacing={2}>
+          <Grid item xs={6} sm={6} display="flex" justifyContent="center">
+            <Link to="/borrowers/signup">
+              <img
+                src="https://res.cloudinary.com/dxeoesm7e/image/upload/v1728581611/Add_a_heading_500_x_500_px_3_w06vz8.png"
+                alt="Borrowers"
+                style={{ width: 600, height: 600, borderRadius: 8 }}
+              />
+            </Link>
+          </Grid>
+          <Grid item xs={6} sm={6} display="flex" justifyContent="center">
+            <Link to="/lenders/signup">
+              <img
+                src="https://res.cloudinary.com/dxeoesm7e/image/upload/v1728583550/Add_a_heading_500_x_500_px_4_zs06zy.png"
+                alt="Lenders"
+                style={{ width: 600, height: 600, borderRadius: 8 }}
+              />
+            </Link>
+          </Grid>
+        </Grid>
+      </Grid>
+      {/* placement for above section */}
+      <Grid container spacing={4} justifyContent="center" sx={{ padding: 18 }}>
+        <Grid item xs={12} display="flex" justifyContent="center">
+          <img
+            src="https://res.cloudinary.com/dxeoesm7e/image/upload/v1727973657/How_This_Works_1_i0lklb.png"
+            alt="How This Works Image Placeholder"
+            style={{ width: "100%", maxWidth: 1200, borderRadius: 8 }}
+          />
+        </Grid>
+
+        <Grid container justifyContent="center" spacing={2}>
+          <Grid item xs={4} sm={4} display="flex" justifyContent="center">
+            <img
+              src="https://res.cloudinary.com/dxeoesm7e/image/upload/v1727974119/Add_a_heading_500_x_500_px_u3hahd.png"
+              alt="How it works step 1"
+              style={{ width: 400, height: 400, borderRadius: 8 }}
+            />
+          </Grid>
+          <Grid item xs={4} sm={4} display="flex" justifyContent="center">
+            <img
+              src="https://res.cloudinary.com/dxeoesm7e/image/upload/v1727974117/Add_a_heading_500_x_500_px_1_hl4tvp.png"
+              alt="How it works step 2"
+              style={{ width: 400, height: 400, borderRadius: 8 }}
+            />
+          </Grid>
+          <Grid item xs={4} sm={4} display="flex" justifyContent="center">
+            <img
+              src="https://res.cloudinary.com/dxeoesm7e/image/upload/v1727974118/Add_a_heading_500_x_500_px_2_f3zlzp.png"
+              alt="How it works step 3"
+              style={{ width: 400, height: 400, borderRadius: 8 }}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={4} justifyContent="center" sx={{ padding: 5 }}>
         <Grid item xs={12} display="flex" justifyContent="center">
           <img
             src="https://res.cloudinary.com/dxeoesm7e/image/upload/v1728576656/How_This_Works_2_tg9j17.png"
