5df16a5d40545cb3ae320f8d813c5f3912495cd7
manoelteixeira <manoel.teixeira.cc@gmail.com>
2024-10-28 11:31:09 -0400

    FIX: profile image
    Signed-off-by: manoelteixeira <manoel.teixeira.cc@gmail.com>

--- src/components/NavBarFolder/ProfileMenuIcon.jsx
+++ src/components/NavBarFolder/ProfileMenuIcon.jsx
@@ -7,43 +7,24 @@ const ProfileMenuIcon = ({ setUser, setToken }) => {
   const [anchorEl, setAnchorEl] = useState(null);
   const [userType, setUserType] = useState("");
   const [profilePic, setProfilePic] = useState("");
+  const [localUser, setLocalUser] = useState(
+    JSON.parse(localStorage.getItem("credentials"))
+  );
+
   const navigate = useNavigate();
 
-  const DEFAULT_IMAGE = "https://picsum.photos/200/300"; 
+  const DEFAULT_IMAGE = "https://picsum.photos/200/300";
 
   const handleMenuOpen = (event) => setAnchorEl(event.currentTarget);
   const handleMenuClose = () => setAnchorEl(null);
 
   useEffect(() => {
-    const fetchProfileData = async () => {
-      try {
-        const credentials = JSON.parse(localStorage.getItem("credentials"));
-        if (!credentials) return;
-
-        setUserType(credentials.user_type);
-
-        
-        const response = await fetch(`${API}/${credentials.user_type}/profile-pic`, {
-          headers: { Authorization: `Bearer ${credentials.token}` },
-        });
-
-        if (response.ok) {
-          const data = await response.json();
-          setProfilePic(data.imageUrl || DEFAULT_IMAGE); 
-        } else {
-          setProfilePic(DEFAULT_IMAGE); 
-        }
-      } catch (error) {
-        console.error("Error fetching profile data:", error);
-        setProfilePic(DEFAULT_IMAGE); 
-      }
-    };
-
-    fetchProfileData();
-  }, []);
+    setProfilePic(localUser.user.image_url || DEFAULT_IMAGE);
+  }, [localUser]);
 
   const handleUpdateProfileClick = () => {
-    const route = userType === "borrower" ? "/borrowers/:id/edit" : "/lenders/:id/edit";
+    const route =
+      userType === "borrower" ? "/borrowers/:id/edit" : "/lenders/:id/edit";
     navigate(route);
     handleMenuClose();
   };
@@ -61,7 +42,11 @@ const ProfileMenuIcon = ({ setUser, setToken }) => {
       <IconButton onClick={handleMenuOpen} sx={{ ml: 2 }}>
         <Avatar src={profilePic} alt="Profile Picture" />
       </IconButton>
-      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+      >
         <MenuItem onClick={handleUpdateProfileClick}>Update Profile</MenuItem>
         <MenuItem onClick={handleLogoutClick}>Log Out</MenuItem>
       </Menu>
