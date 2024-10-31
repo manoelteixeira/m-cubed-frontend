b20cca89233694d177f27d265490312605426a7d
manoelteixeira <manoel.teixeira.cc@gmail.com>
2024-10-31 17:54:35 -0400

    UPDATE: Borrower Profile
    Signed-off-by: manoelteixeira <manoel.teixeira.cc@gmail.com>

--- src/Pages/BorrowerProfile.jsx
+++ src/Pages/BorrowerProfile.jsx
@@ -6,12 +6,11 @@ import {
   Typography,
   Box,
   Grid,
+  Paper,
+  Stack,
 } from "@mui/material";
 import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
-import Paper from "@mui/material/Paper";
-import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
-
 import { useState } from "react";
 
 const API = import.meta.env.VITE_BASE_URL;
@@ -42,7 +41,6 @@ export default function BorrowerProfile({ user, token }) {
         setProfile(borrower);
         setCreditReport(credit_reports[0]);
       })
-
       .catch((err) => console.log(err));
   }, [user]);
 
@@ -50,155 +48,158 @@ export default function BorrowerProfile({ user, token }) {
     return <>Loading...</>;
   } else {
     return (
-      <div className="borrower-profile">
-        <Box
-          sx={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
-        >
-          <Grid container spacing={5} sx={{ p: 10 }}>
-            {/* Profile Picture and Name */}
-            <Grid item xs={12} md={3}>
-              <Card
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  p: 3,
-                }}
-              >
-                <Avatar
-                  src={profile.image_url}
-                  alt={profile.business_name}
-                  sx={{ width: 120, height: 120, mb: 2 }}
-                />
-                <Typography variant="h5" gutterBottom>
-                  <strong>{profile.business_name}</strong>
-                </Typography>
-              </Card>
-            </Grid>
+      <Box
+        sx={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: "#f4f6f8",
+          p: 5,
+        }}
+      >
+        <Grid container spacing={4} sx={{ p: 2 }}>
+          {/* Profile Picture and Name */}
+          <Grid item xs={12} md={3}>
+            <Card
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                p: 3,
+                boxShadow: 3,
+              }}
+            >
+              <Avatar
+                src={profile.image_url}
+                alt={profile.business_name}
+                sx={{ width: 120, height: 120, mb: 2 }}
+              />
+              <Typography variant="h5" gutterBottom>
+                <strong>{profile.business_name}</strong>
+              </Typography>
+            </Card>
+          </Grid>
 
-            {/* Business Information */}
-            <Grid item xs={12} md={8}>
-              <Card>
-                <CardContent>
-                  <Typography variant="h4" textAlign={"center"} gutterBottom>
-                    Business Information
-                  </Typography>
-                  <Grid container spacing={1}>
-                    <Grid item xs={6}>
-                      <Typography variant="body1">
-                        <strong>City:</strong> {profile.city}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography variant="body1">
-                        <strong>Street:</strong> {profile.street}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography variant="body1">
-                        <strong>State:</strong> {profile.state}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography variant="body1">
-                        <strong>Zip Code:</strong> {profile.zip_code}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography variant="body1">
-                        <strong>Phone:</strong> {profile.phone}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography variant="body1">
-                        <strong>EIN:</strong> {profile.ein}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography variant="body1">
-                        <strong>Start Date:</strong>{" "}
-                        {new Date(profile.start_date).toLocaleDateString()}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography variant="body1">
-                        <strong>Industry:</strong> {profile.industry}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography variant="body1">
-                        <strong>Email:</strong> {profile.email}
-                      </Typography>
-                    </Grid>
+          {/* Business Information */}
+          <Grid item xs={12} md={8}>
+            <Card sx={{ p: 3, boxShadow: 3 }}>
+              <CardContent>
+                <Typography variant="h4" textAlign="center" gutterBottom>
+                  Business Information
+                </Typography>
+                <Grid container spacing={2}>
+                  <Grid item xs={6}>
+                    <Typography variant="body1">
+                      <strong>City:</strong> {profile.city}
+                    </Typography>
                   </Grid>
-                </CardContent>
-              </Card>
-            </Grid>
-            {/* Credit Report */}
-            <Grid item xs={15} md={5}>
-              <Card sx={{ padding: 5 }}>
-                <Typography variant="h4" textAlign={"center"} gutterBottom>
+                  <Grid item xs={6}>
+                    <Typography variant="body1">
+                      <strong>Street:</strong> {profile.street}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="body1">
+                      <strong>State:</strong> {profile.state}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="body1">
+                      <strong>Zip Code:</strong> {profile.zip_code}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="body1">
+                      <strong>Phone:</strong> {profile.phone}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="body1">
+                      <strong>EIN:</strong> {profile.ein}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="body1">
+                      <strong>Start Date:</strong>{" "}
+                      {new Date(profile.start_date).toLocaleDateString()}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="body1">
+                      <strong>Industry:</strong> {profile.industry}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="body1">
+                      <strong>Email:</strong> {profile.email}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          {/* Credit Report */}
+          <Grid item xs={12} md={6}>
+            <Card
+              sx={{ display: "flex", alignItems: "center", p: 3, boxShadow: 3 }}
+            >
+              <Box textAlign="center">
+                <Typography variant="h4" gutterBottom>
                   Credit Report
                 </Typography>
-                <Box display="flex" alignItems="center">
-                  <Gauge
-                    width={200}
-                    height={200}
-                    value={creditReport.score}
-                    startAngle={-110}
-                    endAngle={110}
-                    valueMin={300}
-                    valueMax={850}
-                    sx={{
-                      [`& .${gaugeClasses.valueText}`]: {
-                        fontSize: 25,
-                        transform: "translate(0px, -10px)",
-                      },
-                    }}
-                    text={({ value, valueMax }) => `${value} / ${valueMax}`}
-                  />
-
-                  <CardContent>
-                    <Typography variant="body1">
-                      <strong>Credit Bureau:</strong>{" "}
-                      {creditReport.credit_bureau}
-                    </Typography>
-
-                    <Typography variant="body1">
-                      <strong>Report ID:</strong> #{creditReport.report_id}
-                    </Typography>
-
-                    <Typography variant="body1">
-                      <strong>Created At:</strong>{" "}
-                      {new Date(creditReport.created_at).toLocaleDateString()}
-                    </Typography>
-
-                    <Typography variant="body1">
-                      <strong>Expire At:</strong>{" "}
-                      {new Date(creditReport.expire_at).toLocaleDateString()}
-                    </Typography>
-                  </CardContent>
-                </Box>
-              </Card>
-            </Grid>
-            {/* Documents */}
-            <Grid item xs={15} md={5}>
-              <Card sx={{ padding: 5 }}>
-                <Typography variant="h4" textAlign={"center"} gutterBottom>
-                  Documents
+                <Gauge
+                  width={200}
+                  height={200}
+                  value={creditReport.score}
+                  startAngle={-110}
+                  endAngle={110}
+                  valueMin={300}
+                  valueMax={850}
+                  sx={{
+                    [`& .${gaugeClasses.valueText}`]: {
+                      fontSize: 25,
+                      transform: "translate(0px, -10px)",
+                    },
+                  }}
+                  text={({ value, valueMax }) => `${value} / ${valueMax}`}
+                />
+              </Box>
+              <CardContent sx={{ textAlign: "center" }}>
+                <Typography variant="body1">
+                  <strong>Credit Bureau:</strong> {creditReport.credit_bureau}
                 </Typography>
-                <Box display="flex" alignItems="center">
-                  <Stack spacing={2}>
-                    <Item>Item 1</Item>
-                    <Item>Item 2</Item>
-                    <Item>Item 3</Item>
-                  </Stack>
-                </Box>
-              </Card>
-            </Grid>
+                <Typography variant="body1">
+                  <strong>Report ID:</strong> #{creditReport.report_id}
+                </Typography>
+                <Typography variant="body1">
+                  <strong>Created At:</strong>{" "}
+                  {new Date(creditReport.created_at).toLocaleDateString()}
+                </Typography>
+                <Typography variant="body1">
+                  <strong>Expire At:</strong>{" "}
+                  {new Date(creditReport.expire_at).toLocaleDateString()}
+                </Typography>
+              </CardContent>
+            </Card>
           </Grid>
-        </Box>
-      </div>
+
+          {/* Documents */}
+          <Grid item xs={12} md={6}>
+            <Card sx={{ p: 3, boxShadow: 3 }}>
+              <Typography variant="h4" textAlign="center" gutterBottom>
+                Documents
+              </Typography>
+              <Box textAlign="center">
+                <Stack spacing={2} sx={{ mt: 2 }}>
+                  <Item>Drives Liscense</Item>
+                  <Item>Secretary of State Report</Item>
+                </Stack>
+              </Box>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
     );
   }
 }
